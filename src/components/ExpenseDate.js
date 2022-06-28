fd0d85b5e61687130f10f4b9e5cf534ba14a6e37
rajpranit <rajpranit1207@gmail.com>
2022-06-28 10:16:05 +0530

    Expenses props

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const date = props.date.toLocaleString("en-US", { day: "numeric" });
+  const year = props.date.toLocaleString("en-US", { year: "numeric" });
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{date} </div>
+      <div className="expense-date__year">{year} </div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+    const expense = props.expense
+
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      />
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={props.expense[1].date}
+      />
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      />
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
