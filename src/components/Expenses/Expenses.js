8a6224603f7c75b8a22e434de7349cedafcafd1a
rajpranit <rajpranit1207@gmail.com>
2022-07-11 23:24:12 +0530

    Conditional Component Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,28 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 
 function Expenses(props) {
+  const [filterDate, setFilterDate] = useState("2020");
 
-  const [filterDate , setFilterDate] = useState('2020')
-
-  const ExpenseFilterDate = filterDate =>{
-      setFilterDate(filterDate)
-  }
+  const ExpenseFilterDate = (filterDate) => {
+    setFilterDate(filterDate);
+  };
 
   const filteredDate = props.expense.filter((ex) => {
     return ex.date.getFullYear().toString() === filterDate;
-  })
+  });
+
+  let expenseContent  = <p>No Expense Found</p>;
+
+  if (filteredDate.length > 0){
+      expenseContent = filteredDate.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          key={expense.id}
+        />
+      ))
+  }
 
   return (
     <>
       <Card className="expenses">
-      <ExpenseFilter selectedYear={filterDate} onFilterDate={ExpenseFilterDate}/>
-      {filteredDate.map((expense) => (
-        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
-      ))}
+        <ExpenseFilter
+          selectedYear={filterDate}
+          onFilterDate={ExpenseFilterDate}
+        />
+        {expenseContent}
       </Card>
     </>
   );
